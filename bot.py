222b2e88c2ffacf77cc286b1c5cc72a7fb497848
fomam4257819 <fomamo4303@proton.me>
2026-04-16 14:05:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ TOKEN        = os.getenv("TELEGRAM_BOT_TOKEN", "ТВІЙ_ТОКЕН_БОТА")
 ADMIN_ID     = int(os.getenv("ADMIN_ID", "887078537"))
 WEBHOOK_URL  = os.getenv("WEBHOOK_URL", "https://78655.onrender.com")
 TURSO_URL    = os.getenv("TURSO_URL",   "https://1qaz2wsx-yhbvgt65.aws-eu-west-1.turso.io")
-TURSO_TOKEN  = os.getenv("TURSO_TOKEN", "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9...")
+TURSO_TOKEN  = os.getenv("TURSO_TOKEN", "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE4MDc4NjA1NDEsImlhdCI6MTc3NjMyNDU0MSwiaWQiOiIwMTlkOTUyZC03YjAxLTc3N2QtYjE4NS03MDEzY2JjOWYwMDkiLCJyaWQiOiI3NmJlZDlhMy01Zjk1LTQ0OGYtYThkYi1kZTY2OTNmNjcwZTAifQ.fN9MZ5inviHOnUNqhrW20hbt1oUmHS6E2auA_grZ6pcv02NvEKEmrI5Ms_oSnwbBM1nTsR-TmE7SSIrB4utKDw")
 
 MAX_DB_RETRIES = 3
 DB_RETRY_DELAY = 2
